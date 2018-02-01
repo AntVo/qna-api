2bc35cde5070541275782d59570e8b2e4fdc46a3
antoinevo <antoine.monarch@gmail.com>
2018-01-31 23:20:38 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+
+
+var data = {
+	"questions": [
+		{"id":1, "question": "What are good places to eat?", "answers":[
+			{"id":1,"answer":"Chipotle"},
+			{"id":2,"answer":"Subways"}	
+		]},
+		{"id":2, "question": "What is the meaning of life?", "answers":[
+			{"id":1,"answer":"Cats"},
+			{"id":2,"answer":"Pizza"}
+		]}
+	]
+	// data.questions[0].question -> What are good places to eat?
+	// data.questions[0].answers[0].answer -> Chipotle
+}
+
+var server = app.listen(3000, () =>{
+	console.log('Listening on port: 3000');
+})
+
+app.use(express.static('public'));
+
+app.get('/questions', getQuestions);
+function getQuestions(req, res){
+	var questions = data.questions.map(item => item.question);
+	res.send(questions);
+}
+
+app.get('/questions/:id/', getAnswers); 
+function getAnswers(req, res){
+	var questionID = req.params.id;
+	var questionAnswers = data.questions[questionID].answers.map(item=> item.answer);
+	res.send(questionAnswers);
+}
